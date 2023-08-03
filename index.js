a65fc0736973b7dfe2856564acd5cfcb3f74ccfa
kmkuiken <kmkuiken@gmail.com>
2023-08-02 22:37:52 -0400

    Wrote game function that plays game
    Function checks both player and computer choices and determines if the
    player has won the game or not.  The function returns an int depending
    on the result of the game.

--- index.js
+++ index.js
@@ -12,3 +12,46 @@ function getComputerChoice() {
 					return "Scissors";
     }
 }
+
+function game(playerChoice, computerChoice) {
+  //Player win = 1, player loss = 0, tie = 2
+
+  if (playerChoice == "Rock") {
+    switch (computerChoice) {
+      case "Rock":
+        return 2;
+      
+      case "Paper":
+        return 0;
+
+      case "Scissors":
+        return 1;
+    }
+  }
+
+  if (playerChoice == "Paper") {
+    switch (computerChoice) {
+      case "Rock":
+        return 1;
+      
+      case "Paper":
+        return 2;
+
+      case "Scissors":
+        return 0;
+    }
+  }
+
+  if (playerChoice == "Scissors") {
+    switch (computerChoice) {
+      case "Rock":
+        return 0;
+      
+      case "Paper":
+        return 1;
+
+      case "Scissors":
+        return 2;
+    }
+  }
+}
