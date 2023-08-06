20ef5f83001a78c5af37b4aebb2d0e960b9b48db
kmkuiken <kmkuiken@gmail.com>
2023-08-05 23:03:43 -0400

    Styling
    Untabbed the getComputerChoice function

--- game.js
+++ game.js
@@ -13,18 +13,18 @@ const scissorsPhoto = document.querySelector(".scissors img");
 
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
+  let choice = Math.floor(Math.random() * 3);
 
-    switch (choice){
-        case 0:
-					return "rock";
+  switch (choice){
+    case 0:
+      return "rock";
 
-				case 1:
-					return "paper";
+    case 1:
+      return "paper";
 
-				case 2:
-					return "scissors";
-    }
+    case 2:
+      return "scissors";
+  }
 }
 
 
