1fe697640fcc8107770b2b6aa77631fdd080452d
kmkuiken <kmkuiken@gmail.com>
2023-08-11 13:21:45 -0400

    Styling changes
    Added box shadow on hover and constantly on gameboard, changed text
    font, and added specific text sizing for mobile

--- game.js
+++ game.js
@@ -137,34 +137,3 @@ function game(playerChoice) {
 rock.addEventListener("click", () => game("rock"));
 paper.addEventListener("click", () => game("paper"));
 scissors.addEventListener("click", () => game("scissors"));
-
-// Mouse over effect for the rock selector, changing the color to orange when over top
-rock.addEventListener("mouseover", () => {
-  rock.style.cssText = "background-color: #D5573B; border-color: #D5573B;";
-  rockPhoto.style.cssText = "background-color: #D5573B; border-color: #D5573B;";
-});
-rock.addEventListener("mouseleave", () => {
-  rock.style.cssText = "background-color: #F7ECE1; border-color: #F7ECE1;";
-  rockPhoto.style.cssText = "background-color: #F7ECE1; border-color: #F7ECE1;";
-});
-
-// Mouse over effect for paper
-paper.addEventListener("mouseover", () => {
-  paper.style.cssText = "background-color: #D5573B; border-color: #D5573B;";
-  paperPhoto.style.cssText = "background-color: #D5573B; border-color: #D5573B;";
-});
-paper.addEventListener("mouseleave", () => {
-  paper.style.cssText = "background-color: #F7ECE1; border-color: #F7ECE1;";
-  paperPhoto.style.cssText = "background-color: #F7ECE1; border-color: #F7ECE1;";
-});
-
-// Mouse over effect for scissors
-scissors.addEventListener("mouseover", () => {
-  scissors.style.cssText = "background-color: #D5573B; border-color: #D5573B;";
-  scissorsPhoto.style.cssText = "background-color: #D5573B; border-color: #D5573B;";
-});
-scissors.addEventListener("mouseleave", () => {
-  scissors.style.cssText = "background-color: #F7ECE1; border-color: #F7ECE1;";
-  scissorsPhoto.style.cssText = "background-color: #F7ECE1; border-color: #F7ECE1;";
-});
-
